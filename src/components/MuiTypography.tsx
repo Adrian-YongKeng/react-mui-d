392b22a1b4221dc3ed35fd3ce568e8d729a2b774
Adrian <yy_1123@hotmail.com>
2025-07-15 17:01:09 +0800

    first commit

---
+++ src/components/MuiTypography.tsx
@@ -0,0 +1,37 @@
+import { Typography } from "@mui/material";
+import React from "react";
+
+const MuiTypography = () => {
+  return (
+    <div>
+      <Typography variant="h1">h1 Heading</Typography>
+      <Typography variant="h2">h2 Heading</Typography>
+      <Typography variant="h3">h3 Heading</Typography>
+      <Typography variant="h4" component="h1" gutterBottom>
+        h4 Heading
+      </Typography>
+      <Typography variant="h5">h5 Heading</Typography>
+      <Typography variant="h6">h6 Heading</Typography>
+
+      {/* h6 element different style */}
+      <Typography variant="subtitle1">Sub title 1</Typography>
+      <Typography variant="subtitle2">Sub title 2</Typography>
+
+      {/* paragraph text, default: variant="body1" */}
+      <Typography>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit quod
+        sequi rem quam voluptas non laborum nostrum unde impedit, blanditiis
+        tempore beatae doloribus cum possimus eveniet placeat voluptatem
+        doloremque accusamus.
+      </Typography>
+      <Typography variant="body2">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sequi
+        sapiente, cupiditate minima nam vel ducimus. Ipsum, fugiat repellat,
+        molestias repellendus distinctio error ratione impedit quos asperiores
+        fuga minus aut dicta.
+      </Typography>
+    </div>
+  );
+};
+
+export default MuiTypography;
