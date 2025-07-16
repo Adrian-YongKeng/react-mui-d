ba4bfa07a17dc95895bf701a0e7dc98ab403bd86
Adrian <yy_1123@hotmail.com>
2025-07-16 16:59:12 +0800

    mui switch component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import "./App.css";
-import MuiCheckbox from "./components/MuiCheckbox";
+// import MuiCheckbox from "./components/MuiCheckbox";
 // import MuiButton from "./components/MuiButton";
 // import MuiTypography from "./components/MuiTypography";
 // import MuiTextField from "./components/MuiTextField";
 // import MuiSelect from "./components/MuiSelect";
 // import MuiRadioButton from "./components/MuiRadioButton";
+import MuiSwitch from "./components/MuiSwitch";
 
 function App() {
   return (
@@ -17,7 +18,9 @@ function App() {
 
       {/* <MuiRadioButton /> */}
 
-      <MuiCheckbox />
+      {/* <MuiCheckbox /> */}
+
+      <MuiSwitch />
     </div>
   );
 }
---
+++ src/components/MuiSwitch.tsx
@@ -0,0 +1,29 @@
+import { Box, FormControlLabel, Switch } from "@mui/material";
+import React, { useState } from "react";
+
+const MuiSwitch = () => {
+  const [checked, setChecked] = useState(false);
+  console.log({ checked });
+
+  const onHandleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setChecked(event.target.checked);
+  };
+
+  return (
+    <Box>
+      <FormControlLabel
+        label="Dark Mode"
+        control={
+          <Switch
+            checked={checked}
+            onChange={onHandleChange}
+            size="small"
+            color="success"
+          />
+        }
+      />
+    </Box>
+  );
+};
+
+export default MuiSwitch;
