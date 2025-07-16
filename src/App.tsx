68ae752c1d8750103ee8118e1eba6ef9e8cf569d
Adrian <yy_1123@hotmail.com>
2025-07-16 16:12:21 +0800

    Mui Checkbox component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import "./App.css";
+import MuiCheckbox from "./components/MuiCheckbox";
 // import MuiButton from "./components/MuiButton";
 // import MuiTypography from "./components/MuiTypography";
 // import MuiTextField from "./components/MuiTextField";
 // import MuiSelect from "./components/MuiSelect";
-import MuiRadioButton from "./components/MuiRadioButton";
+// import MuiRadioButton from "./components/MuiRadioButton";
 
 function App() {
   return (
@@ -14,7 +15,9 @@ function App() {
       {/* <MuiTextField />
       <MuiSelect /> */}
 
-      <MuiRadioButton />
+      {/* <MuiRadioButton /> */}
+
+      <MuiCheckbox />
     </div>
   );
 }
